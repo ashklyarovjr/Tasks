d1e5dfcfcb22ef6cfc65636a0b3c3321ad3ff2d7
ashklyarovjr <anton.shklyarov@gmail.com>
2015-04-14 20:31:43 +0300

    Task2Methods Updated, prepare to launch

--- src/com/epam/tal5/shkliarov/task2/Task2Executor.java
+++ src/com/epam/tal5/shkliarov/task2/Task2Executor.java
@@ -2,4 +2,7 @@ package com.epam.tal5.shkliarov.task2;
 
 
 public class Task2Executor {
+    public static void main(String[] args) {
+
+    }
 }
--- src/com/epam/tal5/shkliarov/task2/Task2Methods.java
+++ src/com/epam/tal5/shkliarov/task2/Task2Methods.java
@@ -1,5 +1,40 @@
 package com.epam.tal5.shkliarov.task2;
 
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Task2Methods {
+
+    private int[] fullLinearArrayInit(int n) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int[] array = new int[n];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = Integer.parseInt(reader.readLine());
+        }
+        return array;
+    }
+
+    private int[] emptyLinearArrayInit(int n) {
+        int[] array = new int[n];
+        return array;
+    }
+
+    public void longestAndShortestNumber() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter n");
+        int n = Integer.parseInt(reader.readLine());
+        int[] numbersArray = fullLinearArrayInit(n);
+        int[] quantitiesArray = emptyLinearArrayInit(n);
+        for (int i = 0; i < numbersArray.length; i++) {
+            for (double j = (double) numbersArray[i]; j > 0; j /= 10) {
+                ++quantitiesArray[i];
+            }
+        }
+        for (int i = 0; i < quantitiesArray.length; i++) {
+            System.out.print(quantitiesArray[i] + " ");
+        }
+
+    }
 }
