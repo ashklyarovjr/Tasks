bed4c4a621fabc205ca9eb0a34524efd45c685f8
Anton <anton.shklyarov@gmail.com>
2015-04-15 09:58:29 +0300

    almost complete 1 point from Task2

--- src/com/epam/tal5/shkliarov/task2/Task2Executor.java
+++ src/com/epam/tal5/shkliarov/task2/Task2Executor.java
@@ -1,8 +1,11 @@
 package com.epam.tal5.shkliarov.task2;
+import com.epam.tal5.shkliarov.task2.Task2Methods;
 
+import java.io.IOException;
 
 public class Task2Executor {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) throws IOException {
+        Task2Methods.longestAndShortestNumber();
     }
 }
--- src/com/epam/tal5/shkliarov/task2/Task2Methods.java
+++ src/com/epam/tal5/shkliarov/task2/Task2Methods.java
@@ -7,7 +7,7 @@ import java.io.InputStreamReader;
 
 public class Task2Methods {
 
-    private int[] fullLinearArrayInit(int n) throws IOException {
+    private static int[] fullLinearArrayInit(int n) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int[] array = new int[n];
         for (int i = 0; i < array.length; i++) {
@@ -16,25 +16,60 @@ public class Task2Methods {
         return array;
     }
 
-    private int[] emptyLinearArrayInit(int n) {
+    private static int[] emptyLinearArrayInit(int n) {
         int[] array = new int[n];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = 0;
+        }
         return array;
     }
 
-    public void longestAndShortestNumber() throws IOException {
+
+    private static int[] bubbleSort(int[] array) {
+        int t  = 0;
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = i+1; j < array.length; j++) {
+                if (array[i]  <  array[j])
+                {
+                    t = array[i];
+                    array[i] = array[j];
+                    array[j] = t;
+                }
+            }
+        }
+        return array;
+    }
+
+    public static void longestAndShortestNumber() throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Enter n");
         int n = Integer.parseInt(reader.readLine());
+        int longestNum = 0;
+        int shortestNum = 0;
+
         int[] numbersArray = fullLinearArrayInit(n);
         int[] quantitiesArray = emptyLinearArrayInit(n);
         for (int i = 0; i < numbersArray.length; i++) {
-            for (double j = (double) numbersArray[i]; j > 0; j /= 10) {
+            for (double j = (double) numbersArray[i]; j >= 1; j /= 10) {
                 ++quantitiesArray[i];
             }
         }
-        for (int i = 0; i < quantitiesArray.length; i++) {
-            System.out.print(quantitiesArray[i] + " ");
+        int shortest = quantitiesArray[0];
+        int longest = quantitiesArray[0];
+        for (int i = 0; i < quantitiesArray.length - 1; i++) {
+            if (shortest > quantitiesArray[i+1]) {
+                shortestNum = numbersArray[i];
+                shortest = quantitiesArray[i];
+            }
+            else if (longest < quantitiesArray[i+1]) {
+                longestNum = numbersArray[i];
+                longest = quantitiesArray[i];
+            }
         }
 
+        System.out.println(shortestNum + " " + shortest);
+        System.out.println(longestNum + " " + longest);
     }
+
+
 }
